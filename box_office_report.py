cdeaa8cf2adafc86767d60f0481c8c976feae00d
ciresmatthew <cires.matthew@student.ccm.edu>
2026-09-16 16:41:27 -0400

    Add box office revenue calculations and reporting- almost done

--- box_office_report.py
+++ box_office_report.py
@@ -3,5 +3,20 @@
 #Lab -01
 #Week 4 
 #9/11/26
+#box_office_report.py
 movie = input("enter the movie title")
-aticket = int(input())
+aticket = int(input("enter number of adult"))
+cticket=int(input("enter number of child"))
+adultrevenue = aticket * 10
+childrevenue = cticket * 6
+Gbox= adultrevenue + childrevenue
+Netbox=Gbox*0.2
+Amount=Gbox-Netbox
+print("Box office report")
+print("------------------")
+print("movie name :",movie)
+print("adult revenue: $", format(adultrevenue, ".2f"))
+print("child revenue: $", format(childrevenue,".2f"))
+print("gross revenue : $", format(Gbox, ".2f"))
+print("theater amount: $", format(Netbox, ".2f"))
+print("distributor amount: $", format(Amount,".2f"))
